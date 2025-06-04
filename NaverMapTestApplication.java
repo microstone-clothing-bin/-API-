e53eb1e7e9e7acb8620365736bd1f30ece5f4d14
Leejunsusu <seaws4383@naver.com>
2025-06-05 00:59:47 +0900

    commit

---
+++ NaverMapTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.naver_map_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverMapTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverMapTestApplication.class, args);
+	}
+
+}
