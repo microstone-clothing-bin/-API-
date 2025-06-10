536c17a6b2de888a5081fc3cc2d57139bbd8f971
Leejunsusu <seaws4383@naver.com>
2025-06-10 23:05:51 +0900

    first commit

---
+++ src/test/java/com/example/naver_map_test/NaverMapTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.naver_map_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverMapTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
